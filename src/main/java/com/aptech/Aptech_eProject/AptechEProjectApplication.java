469d541720932efee1d00b468244b56a181e993e
Vo Van Minh <minhvovann@gmail.com>
2025-01-14 20:41:39 +0700

    first commt

---
+++ src/main/java/com/aptech/Aptech_eProject/AptechEProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aptech.Aptech_eProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptechEProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptechEProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aptech/Aptech_eProject/AptechEProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptech.Aptech_eProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptechEProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
